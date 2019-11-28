aeaf353b802b5ed89686bcc3330e7248f99e82b3
Eric Ueta <37991851+eric-ueta@users.noreply.github.com>
2019-11-28 20:12:51 -0300

    Update routes

--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,29 @@ Route::get('/', function () {
 });
 
 Route::get('/aeronaves', "AeronaveController@index");
-Route::get('/destinos', "DestinoController@index");
-Route::get('/passageiros', "PassageiroController@index");
-Route::get('/voos', "VooController@index");
-Route::get('/login', "AccountController@loginAction");
+Route::post('/aeronaves/create', "AeronaveController@create");
+Route::put('/aeronaves/edit', "AeronaveController@edit");
+Route::delete('/aeronaves/delete', "AeronaveController@delete");
 
+
+Route::get('/destinos', "DestinoController@index");
+Route::post('/destinos/create', "DestinoController@create");
+Route::put('/destinos/edit', "DestinoController@edit");
+Route::delete('/destinos/delete', "DestinoController@delete");
+
+Route::get('/passageiros', "PassageiroController@index");
+Route::post('/passageiros/create', "PassageiroController@create");
+Route::put('/passageiros/edit', "PassageiroController@edit");
+Route::delete('/passageiros/delete', "PassageiroController@delete");
+
+Route::get('/login', "AccountController@login");
+Route::get('/logout', "AccountController@logout");
+Route::get('/register', "AccountController@register");
+Route::get('/forgotpassword', "AccountController@forgotpassword");
+Route::post('/save', "AccountController@save");
+
+
+Route::get('/voos', "VooController@index");
+Route::post('/voos/create', "VooController@create");
+Route::put('/voos/edit',"VooController@edit");
+Route::delete('/voos/delete',"VooController@delete");
